9aba78cc193510ce7322e90807b17770034323c7
vivekkr880 <vivek123yahoo@gmail.com>
2026-09-13 00:53:10 +0530

    Time: 23 ms (54.3%), Space: 17.4 MB (86.97%) - LeetHub

--- 0018-4sum/0018-4sum.cpp
+++ 0018-4sum/0018-4sum.cpp
@@ -10,12 +10,12 @@ public:
 
         sort(nums.begin(), nums.end());
 
-        for (int i = 0; i < n - 3; i++) {
+        for (int i = 0; i < n; i++) {
 
             if (i > 0 && nums[i] == nums[i - 1])
                 continue;
 
-            for (int j = i + 1; j < n - 2; j++) {
+            for (int j = i + 1; j < n; j++) {
 
                 if (j > i + 1 && nums[j] == nums[j - 1])
                     continue;

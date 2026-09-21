f6f94fa75fc6be4fdff30265edf65da475b1dfc1
vivekkr880 <vivek123yahoo@gmail.com>
2026-09-22 00:10:03 +0530

    Time: 0 ms (100%), Space: 23.2 MB (97.85%) - LeetHub

--- 2239-find-closest-number-to-zero/2239-find-closest-number-to-zero.cpp
+++ 2239-find-closest-number-to-zero/2239-find-closest-number-to-zero.cpp
@@ -13,3 +13,24 @@ public:
         return closest;
     }
 };
+
+// class Solution {
+// public:
+//     int findClosestNumber(vector<int>& nums) {
+
+//         int closest = nums[0];
+
+//         for (int i = 0; i < nums.size(); i++) {
+
+//             if (|nums[i]| < |closest|) { // mod is not use in c++ they not accecpt the mathemathic expression
+//                 closest = nums[i];
+//             }
+
+//             else if (|nums[i]| == |closest| && nums[i] > closest) {
+//                 closest = nums[i];
+//             }
+//         }
+
+//         return closest;
+//     }
+// };
